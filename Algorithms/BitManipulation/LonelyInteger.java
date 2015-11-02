04281c41cbc84ab420119c6a7655397cb7fac089
conradh1 <conradh1@gmail.com>
2015-11-01 15:51:31 -1000

    Improved lonely number problem.

--- Algorithms/BitManipulation/LonelyInteger.java
+++ Algorithms/BitManipulation/LonelyInteger.java
@@ -4,43 +4,44 @@ import java.math.*;
 /*
 Returns the n member of an array that only has one instance (lonely).
 See: https://www.hackerrank.com/challenges/lonely-integer
+There are N integers in an array A. All but one integer occur in pairs. Your task is to find the number that occurs only once.
 
+Input Format
+
+The first line of the input contains an integer N, indicating the number of integers. The next line contains N space-separated integers that form the array A.
+
+Constraints
+
+1≤N<100
+N % 2=1 (N is an odd number)
+0≤A[i]≤100,∀i∈[1,N]
+Output Format
+
+Output S, the number that occurs only once.
 */
 class LonelyInteger {
 
 
 
-static private int isLonley( Hashtable<Integer, Integer> numbers ) {
-  Enumeration members;
-  
-  members = numbers.keys();
-  while( members.hasMoreElements()) {
-    int member = (Integer) members.nextElement();
-    int total = numbers.get(member);
-    if ( total == 0 )
-      return member;  // return the lonely number
-  }
-  return -1;
+static private int isLonley( int[] a ) {
+  int lone = 0;
+
+  for (int i = 0; i < a.length; i++)
+    lone ^= a[i];
+  return lone;
 }
 
 
  public final static void main(String[] args)  {
     
     Scanner scan = new Scanner(System.in);
-    Hashtable<Integer, Integer> numbers  = new Hashtable<Integer, Integer>();
     //System.out.print("Number of tests:");
-    int num = scan.nextInt();  // Get the number of tests.
+    int num = scan.nextInt();  // Get the number of elements
+    int[] numbers = new int[num];
     scan.useDelimiter("[,\\s+]");  //Set the scanner delimiter to \s or ,
-    for (int i = 1; i <= num; i++) {
-        //System.out.print("Next Input");
+    for (int i = 0; i < num; i++) {
         int input = scan.nextInt();
-	if ( numbers.containsKey( input )) {
-	  int total = numbers.get( input );
-          numbers.put( input, ++total );
-	}
-	else {
-	  numbers.put( input, 0 );
-	}
+	numbers[i] = input;
     }
     System.out.println(isLonley( numbers ));
  } //main
